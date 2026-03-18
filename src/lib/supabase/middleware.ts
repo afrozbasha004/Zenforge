2b8461312c3dd307286b51132d656cd17e2b2c7e
afrozbasha004 <bashaafroz004@gmail.com>
2026-03-18 16:06:04 +0530

    Fix type declaration for setAll method

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -15,7 +15,7 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet)any[] {
+        setAll(cookiesToSet:any[]) {
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           );
